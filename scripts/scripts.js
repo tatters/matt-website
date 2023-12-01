574e5188c2f4b763c5414be96dd642ecf7331d48
tatters <152567610+tatters@users.noreply.github.com>
2023-11-30 20:09:33 -0500

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,22 @@
+
+
+//Hamburger menu function
+function hamburger() {
+	var menu = document.
+	getElementById("menu-links");
+	if (menu.style.display === "block") {
+		menu.style.display = "none";
+	}
+	else {
+		menu.style.display = "block";
+		menu.style.backgroundColor = "#71797E";
+	}
+}
+
+
+
+
+
+
+
+
